55c5fe3134364e9b3d5d51965bfb857f68aee9fd
Diego Restrepo <diegoluisr@gmail.com>
2023-04-15 12:13:00 -0500

    Updating everything

--- main.py
+++ main.py
@@ -5,118 +5,151 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
+from collections import Counter
+# import warnings
+# warnings.simplefilter("ignore")
 
 df0 = pd.read_excel('data/Dataparcial1.xlsx')
 
-df0.columns = [
-  'Edad',
-  'Clase de Trabajo',
-  'Capital Final',
-  'Educacion',
-  'Educacion numero',
-  'Estado civil',
-  'Ocupacion',
-  'Relacion',
-  'Raza',
-  'Sexo',
-  'Capital ganado',
-  'Capital perdido',
-  'Horas por Semana',
-  'Pais origen',
-  'Clase',
+# Definimos las constantes para las etiquetas de las columnas
+EDAD = 'Edad'
+CLASE_TRABAJO = 'Clase de Trabajo'
+CAPITAL_FINAL = 'Capital Final'
+EDUCACION = 'Educación'
+EDUCACION_NUMERO = 'Educación Número'
+ESTADO_CIVIL = 'Estado Civil'
+OCUPACION = 'Ocupación'
+RELACION = 'Relación'
+RAZA = 'Raza'
+SEXO = 'Sexo'
+CAPITAL_GANADO = 'Capital Ganado'
+CAPITAL_PERDIDO = 'Capital Perdido'
+HORAS_POR_SEMANA = 'Horas por Semana'
+PAIS_ORIGEN = 'País Origen'
+CLASE = 'Clase'
+INDICE = 'Índice'
+
+GT_50K = 1
+LTE_50K = 0
+
+columns = [
+  EDAD,
+  CLASE_TRABAJO,
+  CAPITAL_FINAL,
+  EDUCACION,
+  EDUCACION_NUMERO,
+  ESTADO_CIVIL,
+  OCUPACION,
+  RELACION,
+  RAZA,
+  SEXO,
+  CAPITAL_GANADO,
+  CAPITAL_PERDIDO,
+  HORAS_POR_SEMANA,
+  PAIS_ORIGEN,
+  CLASE,
 ]
 
-out = ['Fila' + str(item) for item in range (0, df0.shape[0])]
-df0['index'] = out
+fastCols = [
+  EDAD
+]
 
-# df1=df0.copy()
+df0.columns = columns
+
+df0[INDICE] = ['Fila' + str(item) for item in range (0, df0.shape[0])]
+
+for column in df0.select_dtypes(include = 'object'):
+  df0[column] = df0[column].astype('string')
+
 df0.info()
 
-summary = df0.describe()
-print(summary)
+# Limpiamos los datos de CLASE
+df0[CLASE] = df0[CLASE].str.replace('.', '')
 
-histo = df0['Edad'].hist(bins=[20,30,40,50,60,70,80,90,100],rwidth=0.9)
-histo.set(xlabel = 'Edad', ylabel = 'Count')  
-plt.title('Histograma Edad')
-plt.show()
+# Reemplazamos los valores de CLASE por '>50K' = 1, '<=50K' = 0
+df0[CLASE] = df0[CLASE].apply(lambda x: GT_50K if x.strip() == ">50K" else LTE_50K)
 
-histo = df0['Capital Final'].plot(kind = 'hist')
-histo.set(xlabel = 'Capital Final', ylabel = 'Count')  
-plt.title('Histograma Capital Final')
-plt.show()
+##########
+# Limpieza de datos
+##########
 
-histo = df0['Educacion numero'].hist(bins=[2,4,6,8,10,12,14,16],rwidth=0.9)
-histo.set(xlabel = 'Educacion numero', ylabel = 'Count')  
-plt.title('Histograma Educacion numero')
-plt.show()
+# Funcion para reemplazar calores atipicos en variables categoricas usando la moda.
+# df: Dataframe
+# columnA: class column to filter
+# columnB: target column
+# compare: class value to filter
+# value: replacement source
+def replace(df, columnA, columnB, compare, value):
+  subset = df.loc[df[columnA] == compare, column]
+  count = Counter(subset)
+  most_frequent_value = count.most_common()
+  mfv = most_frequent_value[0][0]
+  print(f'El valor categórico más frecuente para subgrupo {compare} de la variable {columnB} es: {mfv}')
+  df[columnB] = np.where((df[columnA] == compare) & (df[columnB] == value), mfv, df[columnB])
 
-histo = df0['Capital ganado'].plot(kind = 'hist')
-histo.set(xlabel = 'Capital ganado', ylabel = 'Count')
-plt.title('Histograma Capital ganado')
-plt.show()
+# Function para eliminar valores NaN
+def impute_nan(df, columnA):
+  df[columnA] = df[columnA].dropna()
 
-histo = df0['Capital perdido'].plot(kind = 'hist')
-histo.set(xlabel = 'Capital perdido', ylabel = 'Count')
-plt.title('Histograma Capital perdido')
-plt.show()
+# Funcion para eliminar valores atipicos usando Winsorization(Winsorizacion).
+def impute_outlier(df, columnA, columnB, compare):
+  subset = df.loc[df[columnA] == compare, columnB:columnA]
+  median = subset[columnB].median().astype(int)
+  q1 = subset.quantile(0.25, numeric_only = True)
+  q3 = subset.quantile(0.75, numeric_only = True)
+  iqr = q3 - q1
+  df[columnB] = np.where((df[columnA] == compare) & (df[columnB] > median + 1.5 * iqr[columnB]), median, df[columnB])
+  df[columnB] = np.where((df[columnA] == compare) & (df[columnB] < median - 1.5 * iqr[columnB]), median, df[columnB])
 
-histo = df0['Horas por Semana'].hist(bins = [20,30,40,50,60,70,80,90,100], rwidth = 0.9)
-histo.set(xlabel = 'Horas por Semana', ylabel = 'Count')
-plt.title('Histograma Horas por Semana')
-plt.show()
+##########################
+# Limpiamos General
+##########################
+for column in columns:
+  if df0[column].dtype == 'string':
+    replace(df0, CLASE, column, GT_50K, ' ?')
+    replace(df0, CLASE, column, LTE_50K, ' ?')
 
-ax = df0.groupby('Clase de Trabajo')['Edad'].nunique().plot(kind = 'barh')
-plt.xlabel('Edad')
-plt.ylabel('Clase de Trabajo')
-plt.show()
+  if df0[column].dtype == 'int64':
+    impute_nan(df0, column)
 
-df0.boxplot(column = ['Edad'])
-plt.title("Diagrama de caja")
-plt.ylabel("Count")
-plt.show()
+for column in columns:
 
-plot0 = px.box(df0,x = 'Edad', title = 'Boxplot variable edad')
-plot0.show()
+  if column == CLASE:
+    continue
 
-df0.boxplot(column = ['Capital Final'])
-plt.title("Diagrama de caja")
-plt.ylabel("Count")
-plt.show()
+  #######################
+  # Gráficos Pre Limpieza
+  #######################
+  if df0[column].dtype == 'int64':
+    # histo = df0[column].hist(bins=[20,30,40,50,60,70,80,90,100],rwidth=0.9)
+    histo = df0[column].hist(rwidth = 0.9)
+    histo.set(xlabel = column, ylabel = 'Cuenta')  
+    plt.title(f'Histograma {column}')
+    plt.show()
 
-plot0 = px.box(df0,x = 'Capital Final', title = 'Boxplot variable Capital Final')
-plot0.show()
+    df0.boxplot(column = [column])
+    plt.title(f'Diagrama de caja para {column}')
+    plt.ylabel("Cuenta")
+    plt.show()
 
-df0.boxplot(column = ['Educacion numero'])
-plt.title("Diagrama de caja")
-plt.ylabel("Count")
-plt.show()
+  ##########################
+  # Limpiamos datos atipicos
+  ##########################
+  if df0[column].dtype == 'int64':
+    impute_outlier(df0, CLASE, column, GT_50K)
+    impute_outlier(df0, CLASE, column, LTE_50K)
 
-plot0 = px.box(df0, x = 'Educacion numero', title = 'Boxplot variable educacion numero')
-plot0.show()
+  ########################
+  # Gráficos Post Limpieza
+  ########################
+  if df0[column].dtype == 'int64':
+    # histo = df0[column].hist(bins=[20,30,40,50,60,70,80,90,100],rwidth=0.9)
+    histo = df0[column].hist(rwidth = 0.9)
+    histo.set(xlabel = column, ylabel = 'Cuenta')  
+    plt.title(f'Histograma {column}')
+    plt.show()
 
-df0.boxplot(column = ['Capital ganado'])
-plt.title("Diagrama de caja")
-plt.ylabel("Count")
-plt.show()
-
-plot0 = px.box(df0, x = 'Capital ganado', title = 'Boxplot variable capital ganado')
-plot0.show()
-
-df0.boxplot(column = ['Capital perdido'])
-plt.title("Diagrama de caja")
-plt.ylabel("Count")
-plt.show()
-
-plot0 = px.box(df0, x = 'Capital perdido', title = 'Boxplot variable capital perdido')
-plot0.show()
-
-df0.boxplot(column = ['Horas por semana'])
-plt.title("Diagrama de caja")
-plt.ylabel("Count")
-plt.show()
-
-plot0 = px.box(df0, x = 'Horas por semana', title = 'Boxplot variable horas por semana')
-plot0.show()
-
-plot0 = px.strip(df0, x = "Edad", y = "Clase de trabajo")
-plot0.show()
+    df0.boxplot(column = [column])
+    plt.title(f'Diagrama de caja para {column}')
+    plt.ylabel("Cuenta")
+    plt.show()
